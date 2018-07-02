917fbd4787ad885d2f847e32332cd446fda385c0
qwembe <qwembe@gmail.com>
2018-07-02 23:37:55 +0300

    Window aplet
    added Jframe and buttons at top. No listeners. requaiers canvas update

--- src/com/etu/ControlPanel.java
+++ src/com/etu/ControlPanel.java
@@ -1,8 +1,35 @@
 package com.etu;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ControlPanel extends JPanel {
-    private final JButton load = new JButton("Load from file");
+
+    private final JButton load = createButton("Load from file");
+    private final JButton start = createButton("Start");
+    private final JButton stop = createButton("Stop");
+    private final JButton resume = createButton("Continue");
+    private final JButton restart = createButton("Restart");
+    private final JButton next = createButton("Next step");
+
+    public ControlPanel() {
+        super(new FlowLayout());
+        add(load);
+        add(start);
+        add(stop);
+        add(resume);
+        add(restart);
+        add(next);
+    }
+
+    private JButton createButton(String text){//, int x, int y) {
+        JButton b = new JButton(text);
+        //left.setBounds(x, y, 30, 30);
+        b.setPreferredSize(new Dimension(100,25));
+        b.setFocusPainted(false);
+        b.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
+        return b;
+    }
+
 
 }
--- src/com/etu/GameLauncher.java
+++ src/com/etu/GameLauncher.java
@@ -9,7 +9,7 @@ public class GameLauncher extends JFrame {
         SwingUtilities.invokeLater(() -> new GameLauncher("A-Star Algorithm").setVisible(true));
     }
 
-    private final Component canvas;
+    private final JPanel canvas;
     private final ControlPanel toolBar;
     private final Component log;
 
@@ -17,17 +17,77 @@ public class GameLauncher extends JFrame {
         super(title);
 
         canvas = new JPanel();
-        canvas.setPreferredSize(new Dimension(100,100));
+        canvas.setLayout(new OverlayLayout(canvas));
+        canvas.setPreferredSize(new Dimension(500, 500));
 
-        toolBar = new JFrame();
+        toolBar = new ControlPanel();
+        toolBar.setPreferredSize(new Dimension(700, 100));
 
         log = new JPanel();
-        log.setPreferredSize(new Dimension(50,50));
+        log.setPreferredSize(new Dimension(50, 50));
 
-        J
+        JPanel rootPanel = new JPanel();
+        rootPanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
 
+        toolBarConfig(c);
+        rootPanel.add(toolBar, c);
 
+        canvasConfig(c);
+        rootPanel.add(canvas,c);
+
+        logConfig(c);
+        rootPanel.add(log,c);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setContentPane(rootPanel);
+        pack();
+        setLocationRelativeTo(null);
 
 
     }
+
+    private void initListeners(){
+
+        /*
+        Controller controller = new Controller();
+
+
+
+        Timer timer = new Timer(50, e -> {
+            controller.viewUpdated();
+            canvas.requestFocus();
+        });
+        timer.setRepeats(true);
+        timer.start();*/
+    }
+
+    private void toolBarConfig(GridBagConstraints c) {
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx = GridBagConstraints.NORTH;
+        c.gridy = GridBagConstraints.WEST;
+        c.gridheight = 1;
+        c.gridwidth = 3;
+        c.anchor = GridBagConstraints.CENTER;
+
+    }
+
+    private void canvasConfig(GridBagConstraints c) {
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx = GridBagConstraints.NORTH;
+        c.gridy = GridBagConstraints.WEST;
+        c.gridheight = 2;
+        c.gridwidth = 2;
+        c.anchor = GridBagConstraints.CENTER;
+    }
+
+    private void logConfig(GridBagConstraints c) {
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx = GridBagConstraints.NORTH;
+        c.gridy = GridBagConstraints.WEST;
+        c.gridheight = 2;
+        c.gridwidth = 1;
+        c.anchor = GridBagConstraints.CENTER;
+    }
+
 }
